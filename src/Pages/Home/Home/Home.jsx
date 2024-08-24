adddb2e2e7c677e3ac60e5f8e1ea505468743896
Tahsin974 <tahsinislam974@gmail.com>
2024-08-24 15:25:23 +0600

    add navber

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,14 @@
+import { Helmet } from "react-helmet";
+import Header from "../Header/Header";
 
 const Home = () => {
     return (
+
         <div>
-            
+            <Helmet>
+            <title>Tourism-Website|Home</title>
+            </Helmet>
+            <Header></Header>
         </div>
     );
 };
